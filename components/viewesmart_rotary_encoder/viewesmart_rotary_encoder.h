d64c8ad6b142077d2e7cbd6607acfc7a468d731a
DIY techie <runemg@gmail.com>
2026-04-04 17:01:10 +0200

    Switch rotary acceleration to adaptive step size

--- components/viewesmart_rotary_encoder/viewesmart_rotary_encoder.h
+++ components/viewesmart_rotary_encoder/viewesmart_rotary_encoder.h
@@ -74,6 +74,7 @@ class VieweSmartRotaryEncoderSensor : public sensor::Sensor, public Component {
   int32_t last_reported_step_count_{0};
   int32_t pending_step_delta_{0};
   uint32_t last_step_publish_ms_{0};
+  uint32_t last_value_change_ms_{0};
   int8_t last_emitted_direction_{0};
   int8_t pending_direction_confirmation_{0};
   uint32_t pending_direction_confirmation_ms_{0};
